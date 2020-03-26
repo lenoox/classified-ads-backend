9c3c38b18fc304e88d8d7f32f5b55bdf130d1e10
lenoox <62141973+lenoox@users.noreply.github.com>
2020-03-26 15:11:53 +0100

    initialized project

---
+++ src/test/java/com/lenoox/classifiedads/ClassifiedAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenoox.classifiedads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
